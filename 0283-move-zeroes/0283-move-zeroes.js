d6ab6dca05130e7fcaa7d80098fd4d41ff5b77ba
Marwa <marwaamjad6578@gmail.com>
2026-02-13 00:43:13 +0500

    Time: 211 ms (5.04%), Space: 61.2 MB (16.01%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+
+            if (nums[i] == 0) {
+                // move all elements to one step left
+                let temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+};
